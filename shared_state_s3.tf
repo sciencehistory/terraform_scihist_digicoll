ba8a0876c6ee1cf560f197e49fa2d2e209c1199b
Jonathan Rochkind <jrochkind@sciencehistory.org>
2021-07-20 15:26:19 -0400

    put remote S3 shared state conditionally only in production config

--- shared_state_s3.tf
+++ shared_state_s3.tf
@@ -4,9 +4,17 @@
 # Obviously we ran this before we actually configured the backend.
 #
 # See: https://mohitgoyal.co/2020/09/30/upload-terraform-state-files-to-remote-backend-amazon-s3-and-azure-storage-account/
+#
+# NOTE: Our use of the "workspace" feature.... since we are using workspaces for separating production
+# and staging, but we only have ONE terraform-state infrastructure, we put it only in production
+# using 'count'.
+#
+# If you want to deploy changes to these with terraform, you have to apply them in `production` workspace.
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "scihist-digicoll-terraform-state"
+
+  count = "${terraform.workspace == "production" ? 1 : 0}"
 
   tags                        = {
       "service" = local.service_tag
@@ -30,7 +38,9 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state[0].id
+
+  count = "${terraform.workspace == "production" ? 1 : 0}"
 
   block_public_acls       = true
   block_public_policy     = true
@@ -40,6 +50,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 
 resource "aws_dynamodb_table" "terraform_state_locks" {
   name         = "scihist-digicoll-terraform-state-locks"
+
+  count = "${terraform.workspace == "production" ? 1 : 0}"
+
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
