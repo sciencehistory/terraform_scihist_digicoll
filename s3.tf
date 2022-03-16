aebee5e595f8e1ac26cc9fbf16360b0dad60e53b
Eddie Rubeiz <erubeiz@sciencehistory.org>
2022-03-16 13:34:26 -0400

    add s3 server access logging to production originals bucket

--- s3.tf
+++ s3.tf
@@ -346,6 +346,15 @@ resource "aws_s3_bucket" "originals" {
     }
 
 
+    # We only have logging turned on for production_originals:
+    dynamic "logging" {
+        for_each = terraform.workspace == "production" ? [1] : []
+        content {
+                target_bucket = "chf-logs"
+                target_prefix = "s3_server_access"
+            }       
+    }
+
     lifecycle_rule {
         enabled                                = true
         id                                     = "Expire previous files"
