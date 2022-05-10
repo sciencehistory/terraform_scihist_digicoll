eb986b27dc73133811afdcdcbf871833de7aff2a
Jonathan Rochkind <jonathan@dnil.net>
2022-05-10 12:32:19 -0400

    CloudFront distribution for video derivatives (HLS)

--- output.tf
+++ output.tf
@@ -3,3 +3,7 @@
 output "RAILS_ASSET_HOST" {
   value = aws_cloudfront_distribution.rails_static_assets.domain_name
 }
+
+output "S3_BUCKET_DERIVATIVES_VIDEO_HOST" {
+  value = aws_cloudfront_distribution.staging-derivatives-video.domain_name
+}
