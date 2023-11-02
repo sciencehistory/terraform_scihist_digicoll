e5885da9c6a1b937da74f6a560c868bcea63d692
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-11-02 16:09:08 -0400

    adding replication roles

--- backup_replication.tf
+++ backup_replication.tf
@@ -19,10 +19,10 @@ resource "aws_iam_role" "S3-Backup-Replication" {
   description           = "Replication role for backing up data between thumbnail bucket in US-EAST and backup bucket in US-WEST. Does not replicate delete commands, so breaks the normal cross-region replication rule there. Deletes must be done manually."
   force_detach_policies = false
   managed_policy_arns = [
-    "arn:aws:iam::335460257737:policy/s3crr_for_scihist-digicoll-production-originals-video_to_scihist-digicoll-production-originals-video-backup",
-    "arn:aws:iam::335460257737:policy/service-role/s3crr_for_scihist-digicoll-production-derivatives_to_scihist-digicoll-production-derivatives-backup",
-    "arn:aws:iam::335460257737:policy/service-role/s3crr_for_scihist-digicoll-production-dzi_to_scihist-digicoll-production-dzi-backup",
-    "arn:aws:iam::335460257737:policy/service-role/s3crr_for_scihist-digicoll-production-originals_to_scihist-digicoll-production-originals-backup",
+    aws_iam_policy.replicate_derivatives.arn,
+    aws_iam_policy.replicate_dzi.arn,
+    aws_iam_policy.replicate_originals.arn,
+    aws_iam_policy.replicate_originals_video.arn
   ]
   max_session_duration = 3600
   name                 = "S3-Backup-Replication"
@@ -31,33 +31,165 @@ resource "aws_iam_role" "S3-Backup-Replication" {
   tags_all             = {}
 }
 
-# Can't actually import these, as the names are too long.
 
-# terraform import aws_iam_role.s3crr_for_scihist-digicoll-production-originals-video_to_scihist-digicoll-production-originals-video-backup s3crr_for_scihist-digicoll-production-originals-video_to_scihist-digicoll-production-originals-video-backup
-# s3crr_for_scihist-digicoll-production-originals-video_to_scihist-digicoll-production-originals-video-backup
-resource "aws_iam_role" "s3crr_for_scihist-digicoll-production-originals-video_to_scihist-digicoll-production-originals-video-backup" {
-  name               = "s3crr_originals_video"
-  assume_role_policy = jsonencode({})
+#  terraform import aws_iam_policy.replicate_originals_video arn:aws:iam::335460257737:policy/replicate_originals_video
+# aws_iam_policy.replicate_originals_video:
+resource "aws_iam_policy" "replicate_originals_video" {
+    description = "Cross-region replication for originals_video"
+    name        = "replicate_originals_video"
+    path        = "/"
+    policy      = jsonencode(
+        {
+            Statement = [
+                {
+                    Action   = [
+                        "s3:Get*",
+                        "s3:ListBucket",
+                    ]
+                    Effect   = "Allow"
+                    Resource = [
+                        "arn:aws:s3:::scihist-digicoll-production-originals-video",
+                        "arn:aws:s3:::scihist-digicoll-production-originals-video/*",
+                    ]
+                },
+                {
+                    Action   = [
+                        "s3:ReplicateObject",
+                        "s3:ReplicateDelete",
+                        "s3:ReplicateTags",
+                        "s3:GetObjectVersionTagging",
+                    ]
+                    Effect   = "Allow"
+                    Resource = "arn:aws:s3:::scihist-digicoll-production-originals-video-backup/*"
+                },
+            ]
+            Version   = "2012-10-17"
+        }
+    )
+    tags        = {}
+    tags_all    = {}
 }
 
-# terraform import aws_iam_role.s3crr_for_scihist-digicoll-production-originals_to_scihist-digicoll-production-originals-backup s3crr_for_scihist-digicoll-production-originals_to_scihist-digicoll-production-originals-backup
-# s3crr_for_scihist-digicoll-production-originals_to_scihist-digicoll-production-originals-backup
-resource "aws_iam_role" "s3crr_for_scihist-digicoll-production-originals_to_scihist-digicoll-production-originals-backup" {
-  name               = "s3crr_originals"
-  assume_role_policy = jsonencode({})
+
+# terraform import aws_iam_policy.replicate_originals arn:aws:iam::335460257737:policy/replicate_originals
+# aws_iam_policy.replicate_originals:
+resource "aws_iam_policy" "replicate_originals" {
+    description = "Cross-region replication for originals"
+    name        = "replicate_originals"
+    path        = "/"
+    policy      = jsonencode(
+        {
+            Statement = [
+                {
+                    Action   = [
+                        "s3:Get*",
+                        "s3:ListBucket",
+                    ]
+                    Effect   = "Allow"
+                    Resource = [
+                        "arn:aws:s3:::scihist-digicoll-production-originals",
+                        "arn:aws:s3:::scihist-digicoll-production-originals/*",
+                        "arn:aws:s3:::scihist-digicoll-production-dzi",
+                        "arn:aws:s3:::scihist-digicoll-production-dzi/*",
+                        "arn:aws:s3:::scihist-digicoll-production-derivatives",
+                        "arn:aws:s3:::scihist-digicoll-production-derivatives/*",
+                    ]
+                },
+                {
+                    Action   = [
+                        "s3:ReplicateObject",
+                        "s3:ReplicateDelete",
+                        "s3:ReplicateTags",
+                        "s3:GetObjectVersionTagging",
+                    ]
+                    Effect   = "Allow"
+                    Resource = [
+                        "arn:aws:s3:::scihist-digicoll-production-originals-backup/*",
+                        "arn:aws:s3:::scihist-digicoll-production-dzi-backup/*",
+                        "arn:aws:s3:::scihist-digicoll-production-derivatives-backup/*",
+                    ]
+                },
+            ]
+            Version   = "2012-10-17"
+        }
+    )
+    tags        = {}
+    tags_all    = {}
 }
 
-# terraform import aws_iam_role.s3crr_for_scihist-digicoll-production-dzi_to_scihist-digicoll-production-dzi-backup s3crr_for_scihist-digicoll-production-dzi_to_scihist-digicoll-production-dzi-backup
-# s3crr_for_scihist-digicoll-production-dzi_to_scihist-digicoll-production-dzi-backup
-resource "aws_iam_role" "s3crr_for_scihist-digicoll-production-dzi_to_scihist-digicoll-production-dzi-backup" {
-  name               = "s3crr_dzi"
-  assume_role_policy = jsonencode({})
+# terraform import aws_iam_policy.replicate_dzi arn:aws:iam::335460257737:policy/replicate_dzi
+
+# aws_iam_policy.replicate_dzi:
+resource "aws_iam_policy" "replicate_dzi" {
+    description = "Cross-region replication for dzi"
+    name        = "replicate_dzi"
+    path        = "/"
+    policy      = jsonencode(
+        {
+            Statement = [
+                {
+                    Action   = [
+                        "s3:Get*",
+                        "s3:ListBucket",
+                    ]
+                    Effect   = "Allow"
+                    Resource = [
+                        "arn:aws:s3:::scihist-digicoll-production-dzi",
+                        "arn:aws:s3:::scihist-digicoll-production-dzi/*",
+                    ]
+                },
+                {
+                    Action   = [
+                        "s3:ReplicateObject",
+                        "s3:ReplicateDelete",
+                        "s3:ReplicateTags",
+                        "s3:GetObjectVersionTagging",
+                    ]
+                    Effect   = "Allow"
+                    Resource = "arn:aws:s3:::scihist-digicoll-production-dzi-backup/*"
+                },
+            ]
+            Version   = "2012-10-17"
+        }
+    )
+    tags        = {}
+    tags_all    = {}
 }
-# terraform import aws_iam_role.s3crr_for_scihist-digicoll-production-derivatives_to_scihist-digicoll-production-derivatives-backup s3crr_for_scihist-digicoll-production-derivatives_to_scihist-digicoll-production-derivatives-backup
-# s3crr_for_scihist-digicoll-production-derivatives_to_scihist-digicoll-production-derivatives-backup
-resource "aws_iam_role" "s3crr_for_scihist-digicoll-production-derivatives_to_scihist-digicoll-production-derivatives-backup" {
-  name = "s3crr_derivatives"
 
-  assume_role_policy = jsonencode({})
-
+# # terraform import aws_iam_policy.replicate_derivatives arn:aws:iam::335460257737:policy/replicate_derivatives
+# aws_iam_policy.replicate_derivatives:
+resource "aws_iam_policy" "replicate_derivatives" {
+    description = "Cross-region replication for derivatives"
+    name        = "replicate_derivatives"
+    path        = "/"
+    policy      = jsonencode(
+        {
+            Statement = [
+                {
+                    Action   = [
+                        "s3:Get*",
+                        "s3:ListBucket",
+                    ]
+                    Effect   = "Allow"
+                    Resource = [
+                        "arn:aws:s3:::scihist-digicoll-production-derivatives",
+                        "arn:aws:s3:::scihist-digicoll-production-derivatives/*",
+                    ]
+                },
+                {
+                    Action   = [
+                        "s3:ReplicateObject",
+                        "s3:ReplicateDelete",
+                        "s3:ReplicateTags",
+                        "s3:GetObjectVersionTagging",
+                    ]
+                    Effect   = "Allow"
+                    Resource = "arn:aws:s3:::scihist-digicoll-production-derivatives-backup/*"
+                },
+            ]
+            Version   = "2012-10-17"
+        }
+    )
+    tags        = {}
+    tags_all    = {}
 }
