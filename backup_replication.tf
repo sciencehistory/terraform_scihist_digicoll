f3462bb72f654fcbab149c25828f51d508486974
Eddie Rubeiz <erubeiz@sciencehistory.org>
2023-11-09 12:40:36 -0500

    breaking up roles

--- backup_replication.tf
+++ backup_replication.tf
@@ -1,36 +1,3 @@
-# aws_iam_role.S3-Backup-Replication:
-resource "aws_iam_role" "S3-Backup-Replication" {
-  count = terraform.workspace == "production" ? 1 : 0
-  assume_role_policy = jsonencode(
-    {
-      Statement = [
-        {
-          Action = "sts:AssumeRole"
-          Effect = "Allow"
-          Principal = {
-            Service = "s3.amazonaws.com"
-          }
-        },
-      ]
-      Version = "2012-10-17"
-    }
-  )
-  description           = "Replication role for backing up data between thumbnail bucket in US-EAST and backup bucket in US-WEST. Does not replicate delete commands, so breaks the normal cross-region replication rule there. Deletes must be done manually."
-  force_detach_policies = false
-  managed_policy_arns = [
-    aws_iam_policy.replicate_derivatives[0].arn,
-    aws_iam_policy.replicate_dzi[0].arn,
-    aws_iam_policy.replicate_originals[0].arn,
-    aws_iam_policy.replicate_originals_video[0].arn
-  ]
-  max_session_duration = 3600
-  name                 = "S3-Backup-Replication"
-  path                 = "/"
-  tags                 = {}
-  tags_all             = {}
-}
-
-
 # aws_iam_policy.replicate_originals_video:
 resource "aws_iam_policy" "replicate_originals_video" {
   count       = terraform.workspace == "production" ? 1 : 0
---
+++ iam_role_mediaconvert_production.tf
@@ -0,0 +1,30 @@
+# aws_iam_role.production_mediaconvert_role:
+resource "aws_iam_role" "production_mediaconvert_role" {
+  count = terraform.workspace == "production" ? 1 : 0
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "mediaconvert.amazonaws.com"
+          }
+          Sid = ""
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+  description           = "Allows MediaConvert service to call S3 APIs and API Gateway on your behalf. PRODUCTION"
+  force_detach_policies = false
+  managed_policy_arns = [
+    aws_iam_policy.bucket_access_from_app_production[0].arn,
+    "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess",
+  ]
+  max_session_duration = 3600
+  name                 = "scihist-digicoll-production-MediaConvertRole"
+  path                 = "/"
+  tags                 = {}
+  tags_all             = {}
+}
