ef80b7dd234634c2633d4c121776c9f67d4d1944
Jonathan Rochkind <jonathan@dnil.net>
2024-06-18 17:54:14 -0400

    move video derivatives cloudfront to config file for video derivatives, new config organization

--- bucket_derivatives_video.tf
+++ bucket_derivatives_video.tf
@@ -21,8 +21,72 @@ resource "aws_s3_bucket" "derivatives_video" {
   }
 }
 
-resource "aws_s3_bucket_cors_configuration" "derivatives_video" {
+# Video-derviatives cloudfront, in front of S3
+# * cheaper price class North America/Europe only
+# * add on cache-control header with far future caches for clients,
+#   since MediaConvert won't write those in our outputs in S3 already
+resource "aws_cloudfront_distribution" "derivatives-video" {
+  comment         = "${terraform.workspace}-derivatives-video S3"
+  enabled         = true
+  is_ipv6_enabled = true
 
+  # North America/Europe only, cheaper price class
+  price_class = "PriceClass_100"
+
+  origin {
+    domain_name = "scihist-digicoll-${terraform.workspace}-derivatives-video.s3.${var.aws_region}.amazonaws.com"
+    origin_id   = "${terraform.workspace}-derivatives-video.s3"
+  }
+
+  # add tag matching bucket name tag used for S3 buckets themselves,
+  # for cost analysis.
+  tags = {
+    "Cloudfront-Distribution-Origin-Id" = "${terraform.workspace}-derivatives-video.s3"
+    "S3-Bucket-Name"                    = "${local.name_prefix}-derivatives-video"
+  }
+
+
+  default_cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS",
+    ]
+
+    # We're already sending mp4 content, adding gzip compression on top
+    # won't help and may hurt.
+    compress = false
+
+    target_origin_id       = "${terraform.workspace}-derivatives-video.s3"
+    viewer_protocol_policy = "https-only"
+
+    # AWS Managed policy for `Managed-CachingOptimizedForUncompressedObjects`
+    cache_policy_id = "b2884449-e4de-46a7-ac36-70bc7f1ddd6d"
+
+    # references policy for far-future Cache-Control header to be added
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.cors-with-preflight-and-long-time-cache.id
+  }
+
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "derivatives_video" {
   bucket = aws_s3_bucket.derivatives_video.id
 
   cors_rule {
